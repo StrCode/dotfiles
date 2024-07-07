a8f5a40dd967ca60476e13a910f70833ef541c88
Samuel Bello <samcodebello@gmail.com>
2024-07-07 21:51:32 +0100

    Added new completions and sorted gitignore and stow ignore

--- zsh/.config/zsh/completions.zsh
+++ zsh/.config/zsh/completions.zsh
@@ -1,12 +1,3 @@
-# matches case insensitive for lowercase
-# zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-# zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-# zstyle ':completion:*' menu no
-# zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-# zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
-
-# Set the directory we want to store zinit and plugins
-ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 
 # Download Zinit, if it's not there yet
 if [ ! -d "$ZINIT_HOME" ]; then
@@ -26,14 +17,20 @@ zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-autosuggestions
 zinit light Aloxaf/fzf-tab
 
+# Add in Snippets
+zinit snippet OMZP::brew
+zinit snippet OMZP::python
+zinit snippet OMZP::1password
+
 # Load completions
 autoload -Uz compinit && compinit
 
 zinit cdreplay -q
 
 # Completion styling
+zstyle ':completion:*' auto-description 'specify: %d'
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
-zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -1 --color=always $realpath'
