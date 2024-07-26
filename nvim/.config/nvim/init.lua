641c42073ab6cd63824bb0d69cdccb3f735eb765
vector <samcodebello@gmail.com>
2024-07-26 07:18:14 +0100

    added rust lsp config to make rust projects work

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -472,7 +472,24 @@ require('lazy').setup({
             },
           },
         },
-        -- rust_analyzer = {}, ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs Some languages (like typescript) have entire language plugins that can be useful: https://github.com/pmizio/typescript-tools.nvim
+
+        rust_analyzer = {
+          cmd = {
+            'rustup',
+            'run',
+            'stable',
+            'rust-analyzer',
+          },
+          filetypes = { 'rust' },
+          settings = {
+            ['rust_analyzer'] = {
+              cargo = {
+                allFeatures = true,
+              },
+            },
+          },
+        },
+        --... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs Some languages (like typescript) have entire language plugins that can be useful: https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
