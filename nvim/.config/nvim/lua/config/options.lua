035f8a6c02a33f4ca58d2e3754fc45389a16f67e
vector <samcodebello@gmail.com>
2024-12-30 01:20:32 +0100

    reset lazyvim to most current work

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,5 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python_host_prog = "/Users/vector/venvs/.nvim-venv/bin/python"
-vim.g.python3_host_prog = "/Users/vector/venvs/.nvim-venv/bin/python"
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -13,4 +13,9 @@ return {
       },
     },
   },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
 }
